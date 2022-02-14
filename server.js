d9d6cc2593666eaf05a5beb690f49d3b9289411c
Vladyslav Makariuk <vadvadm0@gmail.com>
2022-02-14 22:25:32 +0200

    feat - server side quick config & build fix for safari ver. 15.2-15.3

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const port = process.env.PORT || "8080";
+
+// static file serve
+const dirName = "dist";
+
+app.use(express.static(path.resolve(`${dirName}`)));
+
+// not found in static files, so default to index.html
+app.use((req, res) => res.sendFile(path.resolve(`${dirName}/index.html`)));
+
+app.listen(port, (e, b) => {
+  console.log(`Server has started in port ${port}`);
+});
