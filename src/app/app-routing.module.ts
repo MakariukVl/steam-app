4e6dd92efaaab78b51d4ca821f5c57979b3b206a
Vladyslav Makariuk <vadvadm0@gmail.com>
2022-02-15 19:08:03 +0200

    feat - basic routing added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FriendsPageComponent } from '@pages/friends-page/friends-page.component';
+import { GamesPageComponent } from '@pages/games-page/games-page.component';
+import { LibraryPageComponent } from '@pages/library-page/library-page.component';
+import { LoginPageComponent } from '@pages/login-page/login-page.component';
+import { ProfilePageComponent } from '@pages/profile-page/profile-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginPageComponent },
+  { path: 'games', component: GamesPageComponent },
+  { path: 'library', component: LibraryPageComponent },
+  { path: 'friends', component: FriendsPageComponent },
+  { path: 'profile', component: ProfilePageComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
