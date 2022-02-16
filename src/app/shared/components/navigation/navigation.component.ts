57120f51570dc3f9874dc80f5b63b55120cdfd9a
Vladyslav Makariuk <vadvadm0@gmail.com>
2022-02-16 14:29:54 +0200

    feat - added smooth scrolling bechaviour for anchors

--- src/app/shared/components/navigation/navigation.component.ts
+++ src/app/shared/components/navigation/navigation.component.ts
@@ -11,4 +11,8 @@ export class NavigationComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {}
+
+  onAnchorClick() {
+    window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
+  }
 }
