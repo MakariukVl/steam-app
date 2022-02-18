d270ed3f0ca4f1b4898e741b5976d50cee655a35
Vladyslav Makariuk <vadvadm0@gmail.com>
2022-02-18 14:42:15 +0200

    feat - custum checkbox component implemented

--- src/app/shared/components/checkbox-field/checkbox-field.component.ts
+++ src/app/shared/components/checkbox-field/checkbox-field.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-checkbox-field',
   templateUrl: './checkbox-field.component.html',
-  styleUrls: ['./checkbox-field.component.scss']
+  styleUrls: ['./checkbox-field.component.scss'],
 })
 export class CheckboxFieldComponent implements OnInit {
+  /* pseudo - unique random value */
+  generatedId = `checkboxID-${Date.now()}.${Math.random()
+    .toString()
+    .slice(10)}`;
+  @Input() name?: string;
+  @Input() value?: string;
+  @Input() labelText?: string;
+  @Input() checked?: boolean;
+  @Output() checkedChanged = new EventEmitter<boolean>();
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.name = this.name || 'checkbox-group';
+    this.value = this.value || 'checkbox-value';
+    this.labelText = this.labelText || 'Checkbox label';
+    this.checked = this.checked || false;
   }
 
+  onChange(changed: boolean) {
+    this.checked = changed;
+    this.checkedChanged.emit(changed);
+  }
 }
