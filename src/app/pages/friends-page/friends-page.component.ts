ca787a1f46f415c7a6e8df57768305030fe14c0c
Vladyslav Makariuk <vadvadm0@gmail.com>
2022-02-22 17:52:31 +0200

    feat - list of friends on friends-page implemented

--- src/app/pages/friends-page/friends-page.component.ts
+++ src/app/pages/friends-page/friends-page.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { PersonModel } from '@core/models/person.model';
+import { UserCredentialsModel } from '@core/models/user-credentials.model';
 import { AuthService } from '@core/services/auth.service';
 import { FakeUsersService } from '@core/services/fake-users.service';
+import { Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-friends-page',
@@ -8,10 +11,20 @@ import { FakeUsersService } from '@core/services/fake-users.service';
   styleUrls: ['./friends-page.component.scss'],
 })
 export class FriendsPageComponent implements OnInit {
+  isAuthorized: boolean;
+  signedUser: UserCredentialsModel | null;
+  friends$: Observable<PersonModel[]>;
+
   constructor(
     private usersService: FakeUsersService,
     private authService: AuthService
-  ) {}
+  ) {
+    this.isAuthorized = !!authService.signedUser;
+    this.signedUser = authService.signedUser;
+    this.friends$ = this.signedUser
+      ? usersService.getFriends(this.signedUser.id)
+      : of([]);
+  }
 
   onSubmit(search: string) {
     //
